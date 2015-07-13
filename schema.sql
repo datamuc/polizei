869d55e17161a947c6e396a066f52904fcafe5b3
Danijel Tasov <m@rbfh.de>
2015-07-13 20:42:22 +0200

    add index

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
 CREATE VIRTUAL TABLE meldungen_fts using fts4(meldung,title);
 CREATE TABLE meldungen_idx (id char(40), ts datetime, meldung_docid integer);
 CREATE UNIQUE INDEX midx_id on meldungen_idx(id);
+CREATE INDEX midx_ts on meldungen_idx(ts);
